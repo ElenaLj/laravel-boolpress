9c1ed27e598493b6624b83cabc9fd9951a523d2f
ElenaLj <elenaljubijankic@gmail.com>
2022-03-04 10:06:53 +0100

    add email

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Comment;
 use Illuminate\Support\Facades\Validator;
+use App\Mail\CommentMail;
+use Illuminate\Support\Facades\Mail;
 
 
 class CommentController extends Controller
@@ -34,6 +36,8 @@ class CommentController extends Controller
         $newComment->content = $data["content"];
         $newComment->post_id = $data["post_id"];
         $newComment->save();
+
+        Mail::to("webmaster@boolpress.com")->send(new CommentMail($newComment->post));
 
         return response()->json([
             "success" => true
